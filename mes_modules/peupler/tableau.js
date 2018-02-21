a3dd7ef487130d9ce2cd76401d3a1f107edfe826
Yan Nelson-Lapointe <yannl97@hotmail.com>
2018-02-21 10:51:22 -0500

    création des tableaux

--- mes_modules/peupler/tableau.js
+++ mes_modules/peupler/tableau.js
@@ -1,4 +1,4 @@
-"use strict";
+/*"use strict";
 const tabVille = [
 					"Montréal",
 					"Ottawa",
@@ -29,5 +29,50 @@ const tableau = {
 module.exports.tabVille = tabVille;
 module.exports.tabDomaine = tabDomaine;	
 */
+const tabNom = [
+	"Ouellet",
+	"Béland",
+	"Fréchette",
+	"Racicot",
+	"Chouinard",
+	"Davignon",
+	"Breton",
+	"Lacasse",
+	"Tardif", 
+	"Ruest" 
+];
+
+const tabPrenom = [
+	"Franck",
+	"Denis",
+	"Lance",
+	"Étienne",
+	"Yves",
+	"Martine",
+	"Jacqueline",
+	"Nathalie",
+	"Élisabeth",
+	"Sylvie"
+];
+
+const tabTelephone = [
+	"514-931-4943",
+	"514-720-1003",
+	"514-229-9692",
+	"514-573-0021",
+	"514-638-6974",
+	"514-644-7124",
+	"514-435-5178",
+	"514-583-7838",
+	"514-922-4752",
+	"514-846-3822"
+];
+
+const tableau = {
+	tabNom : tabNom,
+	tabPrenom : tabPrenom,
+	tabTelephone : tabTelephone 
+
+}
 
 module.exports = tableau;						
